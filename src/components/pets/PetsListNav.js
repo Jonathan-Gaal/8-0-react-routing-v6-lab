84781700d6e56cdf2e381260cf13157818a4b971
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-11-01 18:17:32 -0400

    need to do pets

--- src/components/pets/PetsListNav.js
+++ src/components/pets/PetsListNav.js
@@ -1,14 +1,23 @@
 import "./PetsListNav.css";
+import { Link, useParams, useNavigate, Navigate } from "react-router-dom";
 
 export const PetsListNav = ({ cats, dogs }) => {
+  let { kind } = useParams(); // destructuring id from data
+  console.log(cats);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/pets/cats");
+  };
+
   return (
     <nav className="pets-nav">
       <ul>
         <li>
-          <a href="/pets/cats">See All Cats ({cats.length})</a>
+          <Link to={`/pets/${useParams}`}>See All Cats ({cats.length})</Link>
         </li>
         <li>
-          <a href="/pets/dogs">See All Dogs ({dogs.length})</a>
+          <Link to={`/pets/${useParams}`}>See All Dogs ({dogs.length})</Link>
         </li>
       </ul>
     </nav>
